97516251d45fbd5b970035f26f22a9c65506ada6
Tony Cheung <iamtonycheung@gmail.com>
2019-02-14 19:59:36 +0800

    After lesson 130

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -8,16 +8,16 @@ class Topic(models.Model):
             return self.top_name
 
 class Webpage(models.Model):
-    topic = models.ForeignKey(Topic)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE,)
     name = models.CharField(max_length=264, unique=True)
     url  = models.URLField(unique=True)
 
     def __str__(self):
         return self.name
 
-class AccessRecord(models.Modle):
-    name = models.ForeignKey(Webpage)
-    date = modes.DateField()
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage, on_delete=models.CASCADE,)
+    date = models.DateField()
 
     def __str__ (self):
         return str(self.date)
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 def index(request):
-    my_dict ={'insert_content':"Hello iam from first app"}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpage_lists=AccessRecord.objects.order_by('date')
+    date_dict ={'access_records':webpage_lists}
+
+    return render(request, 'first_app/index.html', context=date_dict)
